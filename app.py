36df81a103b62408e596693e4f8efd8a4ad2de59
bahromoken <magdiyevbahrom@gmail.com>
2023-09-14 20:30:16 +0500

    Shift to Mysql

--- app.py
+++ app.py
@@ -1,25 +1,61 @@
 import sqlite3 as sql
 from flask import Flask, render_template, redirect, request, url_for, flash
 from werkzeug.exceptions import abort
+import mysql.connector as mysql
+import os
 
 # Configuration
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bahromoken@githubbahrombek'
 
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Mysql connection.l
+def get_mysql_connection():
+    mysql_connection = mysql.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="blog"
+    )
+    return mysql_connection
+    
 # Creating connection with db
-def get_db_connection():
-    conn = sql.connect('database.db')
-    conn.row_factory = sql.Row
-    return conn
+# def get_db_connection():
+#     conn = sql.connect('database.db')
+#     conn.row_factory = sql.Row
+#     return conn
 
+# Showing all posts from data
+@app.route('/index')
+@app.route('/')
+def index():
+    # mysql part
+    connect = get_mysql_connection()
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM posts")
+    posts = cur.fetchall()
+    posts.reverse()
+    connect.close()
+    return render_template('index.html', posts=posts)
+
+    # # Sqlite part
+    # conn = get_db_connection()
+    # posts = conn.execute("SELECT * FROM posts").fetchall()
+    # conn.close()
+    # return render_template('index.html', posts=posts)
 
 def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    conn = get_mysql_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM posts WHERE id = %s',
+                        (post_id,))
+    post = cur.fetchone()
+
     conn.close()
-    if post is None:
+    if cur is None:
         return render_template('index.html')
     return post
 
@@ -30,14 +66,6 @@ def post(post_id):
     return render_template('post.html', post=post)
 
 
-# Showing all posts from data
-@app.route('/index')
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute("SELECT * FROM posts").fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
 
 
 # New page to create posts
@@ -46,15 +74,34 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+        photo = request.files['photo']
+
+
 
         if not title:
             flash('Title is strongly required')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, context) VALUES (?, ?)',(title,content))
+        
+
+        if photo is None:
+            conn = get_mysql_connection()
+            cur = conn.cursor()
+            cur.execute('INSERT INTO posts (title, context) VALUES (?, ?)',(title,content))
             conn.commit()
             conn.close()
             return redirect('index')
+        else:
+    
+            filename = f'post_.{photo.filename}'
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            flash('Post added successfully', 'success')
+            conn = get_mysql_connection()
+            cur = conn.cursor()
+            cur.execute('INSERT INTO posts (title, context, photo) VALUES (%s, %s, %s)',(title,content,filename))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+                
 
     return render_template('create.html')
 
@@ -71,12 +118,13 @@ def edit(id):
         if not title:
             flash('Title is strongly required')
         else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, context = ?'
-                         'WHERE id = ?', (title,content,id))
+            conn = get_mysql_connection()
+            cur = conn.cursor()
+            cur.execute('UPDATE posts SET title = %s, context = %s'
+                         'WHERE id = %s', (title,content,id))
             conn.commit()
             conn.close()
-            return redirect('index')
+            return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
 
@@ -84,12 +132,18 @@ def edit(id):
 @app.route('/<int:id>/delete', methods=['POST',])
 def delete(id):
     post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn = get_mysql_connection()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM posts WHERE id = %s', (id,))
     conn.commit()
     conn.close()
     flash('{} deleted succesfully'.format(post['title']))
     return redirect(url_for('index'))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,3 +1,47 @@
-import flask
+import mysql.connector
 
-print(flask.__version__)
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="blog"
+)
+
+cursor = db.cursor()
+
+def create_table():
+    cursor.execute("""CREATE TABLE posts(
+                   id INT AUTO_INCREMENT PRIMARY KEY, 
+                   title VARCHAR(255) NOT NULL,
+                   context VARCHAR(255) NOT NULL, 
+                   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+                   photo VARCHAR(255) NOT NULL)""")
+
+
+#Changing data type of photo
+#cursor.execute("ALTER TABLE posts MODIFY photo VARCHAR(255)")
+
+# cursor.execute("INSERT INTO posts (title, context) VALUES (%s, %s)", ('Mysql_4', 'Out of context 4 one'))
+# db.commit()
+
+
+# cursor.execute("SELECT * FROM posts")
+
+
+
+def get_post(post_id):
+    conn = db
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM posts WHERE id = %s',
+                        (post_id,))
+    post = cur.fetchone()
+
+    conn.close()
+    return post
+
+
+
+print(get_post(5))
+
+
+
